383b4c5560fd89f330cefaa825b5a106534c4a7a
Harold <harold.vilander@gmail.com>
2020-11-24 09:00:17 -0600

    clean up

--- src/App.js
+++ src/App.js
@@ -5,38 +5,6 @@ import { isCANMessage, isGPSMessage } from "./util/MessageParsing";
 
 const ENDPOINT = "http://127.0.0.1:4001";
 
-
-let gpsMessageCount = 0;
-let canMessageCount = 0;
-let unidentifiedMessageCount = 0;
-let oldestTS = 0;
-let newestTS = 0;
-let deltaT = 0;
-
-const inbound = (message) => {
-  if (message && isGPSMessage(message)) {
-    gpsMessageCount++;
-  } else if (message && isCANMessage(message)) {
-    canMessageCount++;
-  } else {
-    unidentifiedMessageCount++;
-  }
-
-  if (message.ts) {
-    const timestamp = new Date(message.ts).getTime();
-
-    if (oldestTS === 0 || oldestTS > timestamp) {
-        oldestTS = timestamp;
-    }
-
-    if (newestTS > 0 || newestTS < timestamp) {
-      newestTS = timestamp;
-    }
-
-    deltaT = newestTS - oldestTS;
-  }
-}
-
 /**
  * Takes a given duration in ms and returns a string formated to seconds.
  * @param ms duration in ms
@@ -47,25 +15,50 @@ const formatRunTime = (ms) => {
 }
 
 
-function App() {
+const App = () => {
   const [response, setResponse] = useState(0);
-  const [thing, setThing] = useState('0');
+  const [gpsCount, setGPSCount] = useState(0);
+  const [canCount, setCANCount] = useState(0);
+  const [unidentifiedCount, setUnidentifiedCount] = useState(0);
+  const [oldestTS, setOldestTS] = useState(0);
+  const [newestTS, setNewestTS] = useState(0);
 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT, { transport: ['websocket'] });
     socket.on("FromAPI", data => {
-      inbound(data);
       setResponse(data);
     });
   }, []);
 
+  useEffect(() => {
+    if(response && isGPSMessage(response)) {
+      setGPSCount(gpsCount + 1);
+    }  else if (response && isCANMessage(response)) {
+      setCANCount(canCount + 1);
+    } else {
+      setUnidentifiedCount(unidentifiedCount + 1);
+    }
+
+    if (response.ts) {
+      const timestamp = new Date(response.ts).getTime();
+  
+      if (oldestTS === 0 || oldestTS > timestamp) {
+          setOldestTS(timestamp);
+      }
+  
+      if (newestTS > 0 || newestTS < timestamp) {
+        setNewestTS(timestamp);
+      }
+    } 
+  }, [response])
+
   return (
     <div>
-      <Metric title="Total GPS Messages" value={gpsMessageCount}></Metric>
-      <Metric title="Total Can Messages" value={canMessageCount}></Metric>
-      <Metric title="Total Unidentified Messages" value={unidentifiedMessageCount}></Metric>
-      <Metric title="Total Messages" value={gpsMessageCount + canMessageCount + unidentifiedMessageCount}></Metric>
-      <Metric title="Time Range of Timestamps" value={formatRunTime(deltaT)}></Metric>
+      <Metric title="Total GPS Messages" value={gpsCount}></Metric>
+      <Metric title="Total Can Messages" value={canCount}></Metric>
+      <Metric title="Total Unidentified Messages" value={unidentifiedCount}></Metric>
+      <Metric title="Total Messages" value={gpsCount + canCount + unidentifiedCount}></Metric>
+      <Metric title="Time Range of Timestamps" value={formatRunTime(newestTS - oldestTS)}></Metric>
     </div>
   );
 }
